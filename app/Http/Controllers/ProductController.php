13100c9399c6b2d889add19724382ffb30438259
APARNASREEKUMAR <aparnasreekumar1311@gmail.com>
2019-02-27 17:26:31 +0530

    Sixth Product Resource Created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Model\Product;
+use App\Http\Resources\Product\ProductResource;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -14,7 +15,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        // return "Hello";
+        
+        $product= Product::all();
+        echo "<pre>";
+        print_r($product);
+        echo "</pre>";
     }
 
     /**
@@ -46,7 +52,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        // return $product;
+        return new ProductResource($product);
     }
 
     /**
